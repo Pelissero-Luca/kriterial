05d6c112af329a4902539890f63f3f17b0d6af87
Luca <l.pelissero.1360@vallauri.edu>
2022-04-04 15:33:28 +0200

    Account page now started

--- pages/account.js
+++ pages/account.js
@@ -5,11 +5,36 @@ const account = () => {
     return (
         <>
             <div className={style.main}>
-                <section className={style.section}>
-                </section>
-
-                <section className={style.section}>
-                </section>
+                <div className={style.leftSection}>
+                    <div className={style.indexBar}>
+                        <ul className={style.menuLinks}>
+                            <li>
+                            <img src="https://img.icons8.com/fluency-systems-regular/30/ffffff/dog-house.png"/>
+                                <a>General</a>
+                            </li>
+                            <li>
+                            <img src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/30/ffffff/external-like-instagram-flatart-icons-outline-flatarticons.png"/>
+                                <a>Favorited</a>
+                            </li>
+                            <li>
+                            <img src="https://img.icons8.com/ios/30/ffffff/collectibles.png"/>
+                                <a>Collected</a>
+                            </li>
+                            <li>
+                            <img src="https://img.icons8.com/windows/30/ffffff/tags.png"/>
+                                <a>Offers</a>
+                            </li>
+                            <li>
+                            <img src="https://img.icons8.com/pastel-glyph/30/ffffff/list.png"/>
+                                <a>Listing</a>
+                            </li>
+                            <li>
+                            <img src="https://img.icons8.com/external-android-line-2px-amoghdesign/30/ffffff/external-activity-multimedia-line-24px-android-line-2px-amoghdesign.png"/>
+                                <a>Activity</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </>
     )
